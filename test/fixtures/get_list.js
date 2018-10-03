c1974173814dadeae1d35b04d61c354501f99fb8
Henning Vogt <git@henvo.de>
2018-10-03 02:19:23 +0200

    Add specs for GET_LIST and GET_ONE

---
+++ test/fixtures/get_list.js
@@ -0,0 +1,22 @@
+export default {
+  data: [
+    {
+      type: 'user', id: 1, attributes: { name: 'Bob' },
+    },
+    {
+      type: 'user', id: 2, attributes: { name: 'Alice' },
+    },
+    {
+      type: 'user', id: 3, attributes: { name: 'Harry' },
+    },
+    {
+      type: 'user', id: 4, attributes: { name: 'Zoe' },
+    },
+    {
+      type: 'user', id: 5, attributes: { name: 'Jack' },
+    },
+  ],
+  meta: {
+    total: 5,
+  },
+};
--- test/index.js
+++ test/index.js
@@ -1,8 +1,70 @@
-import assert from 'assert';
-import raJsonapiClient from '../src/index';
+import { expect } from 'chai';
+import nock from 'nock';
 
-describe('Test', () => {
-  it('should be true', () => {
-    assert.equal(1, 1);
+import jsonapiClient from '../src/index';
+import getList from './fixtures/get_list';
+import getOne from './fixtures/get_one';
+
+const client = jsonapiClient('http://api.example.com');
+let result;
+
+describe('GET_LIST', () => {
+  beforeEach(() => {
+    nock('http://api.example.com')
+      .get(/users/)
+      .reply(200, JSON.stringify(getList));
+
+    return client('GET_LIST', 'users', {
+      pagination: { page: 1, perPage: 25 },
+    })
+      .then((data) => { result = data; });
   });
+
+  it('returns an object', () => {
+    expect(result).to.be.an('object');
+  });
+
+  it('has a data property', () => {
+    expect(result).to.have.property('data');
+  });
+
+  it('contains the right count of records', () => {
+    expect(result.data).to.have.lengthOf(5);
+  });
+
+  it('contains valid records', () => {
+    expect(result.data).to.deep.include({ id: 1, name: 'Bob' });
+  });
+});
+
+describe('GET_ONE', () => {
+  beforeEach(() => {
+    nock('http://api.example.com')
+      .get('/users/1')
+      .reply(200, JSON.stringify(getOne));
+
+    return client('GET_ONE', 'users', { id: 1 })
+      .then((data) => { result = data; });
+  });
+
+  it('returns an object', () => {
+    expect(result).to.be.an('object');
+  });
+
+  it('has record ID', () => {
+    expect(result.data).to.have.property('id').that.is.equal(1);
+  });
+
+  it('has records attributes', () => {
+    expect(result.data).to.have.property('name').that.is.equal('Bob');
+  });
+});
+
+describe('CREATE', () => {
+});
+
+describe('UPDATE', () => {
+});
+
+describe('DELETE', () => {
 });
