3f8e4004a3fd82f69dedf7e281c209019f69158a
Henning Vogt <git@henvo.de>
2018-10-20 12:05:53 +0200

    Refactor authentication (#3)
    * Refactor authentication
    
    * Fix specs
    
    * Add linting to travis

--- src/initializer.js
+++ src/initializer.js
@@ -3,6 +3,33 @@ import { HttpError } from './errors';
 
 // Handle HTTP errors.
 export default () => {
+  // Request interceptor
+  axios.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      const username = localStorage.getItem('username');
+      const password = localStorage.getItem('password');
+
+      const newConfig = config;
+
+      // When a 'token' is available set as Bearer token.
+      if (token) {
+        newConfig.headers.Authorization = `Bearer ${token}`;
+      }
+
+      // When username and password are available use
+      // as basic auth credentials.
+      if (username && password) {
+        newConfig.auth.username = username;
+        newConfig.auth.passwors = password;
+      }
+
+      return newConfig;
+    },
+    err => Promise.reject(err),
+  );
+
+  // Response interceptor
   axios.interceptors.response.use(
     response => response,
     (error) => {
