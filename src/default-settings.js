1edf20e35bf038f402935cd2df5baeebf139f35f
jcaspian <jeff_caspian@hotmail.com>
2019-05-24 17:37:31 +0800

    Update default-settings.js
    configurable page query

--- src/default-settings.js
+++ src/default-settings.js
@@ -1,5 +1,7 @@
 export default {
   total: 'total',
+  page: 'page[number]',
+  perPage: 'page[size]',
   headers: {
     Accept: 'application/vnd.api+json; charset=utf-8',
     'Content-Type': 'application/vnd.api+json; charset=utf-8',
