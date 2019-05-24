fc0f6bc2f9c73ee63314deb1e6dd1515846dacf0
jcaspian <jeff_caspian@hotmail.com>
2019-05-24 17:41:26 +0800

    Update default-settings.js
    isolate pagination settings

--- src/default-settings.js
+++ src/default-settings.js
@@ -1,7 +1,9 @@
 export default {
   total: 'total',
-  page: 'page[number]',
-  perPage: 'page[size]',
+  pagination: {
+    page: 'page[number]',
+    perPage: 'page[size]',
+  },
   headers: {
     Accept: 'application/vnd.api+json; charset=utf-8',
     'Content-Type': 'application/vnd.api+json; charset=utf-8',
