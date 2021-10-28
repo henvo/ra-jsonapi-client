421342f063eedbc696e411d5a6a9dc637784fe1e
Alvaro Nicoli <xmr.nkr@gmail.com>
2021-10-27 23:03:12 -0300

    fix: empty filter

--- src/filter.js
+++ src/filter.js
@@ -1,11 +1,11 @@
 export const parseIdsFilter = ({ ids }) => (ids.length > 1 ? `any(id,${ids.reduce((acum, cur) => `${acum.length ? `${acum},` : ''}'${cur}'`, '')})` : `eq(id,${ids[0]})`);
 
-export const makeFilter = ({ filter }) => filter?.raw ?? '';
+export const makeFilter = ({ filter }) => filter?.raw;
 
 export const makeReferenceFilter = ({ target, id }) => `eq(${target},'${id}')`;
 
 export const combineFilters = (...filters) => {
-  const [filter1, ...rest] = filters.filter(f => f.length > 1);
+  const [filter1, ...rest] = filters.filter(f => f?.length > 1);
 
   if (!filter1) {
     return undefined;
