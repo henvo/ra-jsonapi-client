364b901f75a4c53e925e1d9b02f3a16a624c84e3
Alvaro Nicoli <xmr.nkr@gmail.com>
2021-10-28 18:38:10 -0300

    fix: missing quotes

--- src/filter.js
+++ src/filter.js
@@ -1,6 +1,6 @@
 import isObject from './utils';
 
-export const parseIdsFilter = ({ ids }) => (ids.length > 1 ? `any(id,${ids.reduce((acum, cur) => `${acum.length ? `${acum},` : ''}'${isObject(cur) ? cur.id : cur}'`, '')})` : `eq(id,${isObject(ids[0]) ? ids[0].id : ids[0]})`);
+export const parseIdsFilter = ({ ids }) => (ids.length > 1 ? `any(id,${ids.reduce((acum, cur) => `${acum.length ? `${acum},` : ''}'${isObject(cur) ? cur.id : cur}'`, '')})` : `eq(id,'${isObject(ids[0]) ? ids[0].id : ids[0]}')`);
 
 export const makeFilter = ({ filter }) => filter?.raw;
 
